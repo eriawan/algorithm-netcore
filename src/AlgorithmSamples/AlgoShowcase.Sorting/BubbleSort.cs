8e9a4222c5de6a945ea9333845fffd6c4561a7d4
Eriawan Kusumawardhono <eriawan@live.com>
2019-10-31 03:12:21 +0700

    make quick sample of bubblesort

---
+++ src/AlgorithmSamples/AlgoShowcase.Sorting/BubbleSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgoShowcase.Sorting
+{
+    public class BubbleSort
+    {
+        public static List<Int32> DoBubbleSort(List<Int32> data)
+        {
+            List<Int32> sortedResult = data;
+            for (int i = 0; i < sortedResult.Count - 1; i++)
+            {
+                for (int j = 0; j < sortedResult.Count - i - 1; j++)
+                {
+                    if (sortedResult[j] > sortedResult[j + 1])
+                    {
+                        var temp = sortedResult[j];
+                        sortedResult[j] = sortedResult[j + 1];
+                        sortedResult[j + 1] = temp;
+                    }
+                }
+            }
+            return sortedResult;
+        }
+    }
+}
---
+++ src/AlgorithmSamples/AlgoShowcase.Sorting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoShowcase.Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sorting demo");
+            var sampleunsorted = new List<Int32>();
+            sampleunsorted.Add(64);
+            sampleunsorted.Add(34);
+            sampleunsorted.Add(25);
+            sampleunsorted.Add(12);
+            sampleunsorted.Add(22);
+            sampleunsorted.Add(11);
+            sampleunsorted.Add(90);
+            Console.WriteLine("Bubble sort");
+            var sampleBubbleSorted = BubbleSort.DoBubbleSort(sampleunsorted);
+            foreach (var item in sampleBubbleSorted)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine(" ");
+        }
+    }
+}
